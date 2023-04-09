9a26376468b98e4a5acb4d261954bbe30d6c11ca
Rajeev Singla <rajeev1.singla@gmail.com>
2023-04-09 04:25:34 -0700

    Added Zapitel logo and Updated PDF Generator

--- pdfgenerator.go
+++ pdfgenerator.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/johnfercher/maroto/pkg/color"
+
 	"github.com/johnfercher/maroto/pkg/consts"
 	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
 )
 
 func generatePDF(folder string, filename string) {
@@ -12,6 +15,7 @@ func generatePDF(folder string, filename string) {
 
 	m := pdf.NewMaroto(consts.Portrait, consts.A4)
 	m.SetPageMargins(20, 10, 10)
+	buildHeading(m)
 
 	err := m.OutputFileAndClose(folder + "/" + filename + ".pdf")
 
@@ -19,4 +23,47 @@ func generatePDF(folder string, filename string) {
 		fmt.Println(err)
 	}
 
+}
+
+func buildHeading(m pdf.Maroto) {
+
+	heading := "Zapitel"
+
+	m.RegisterHeader(func() {
+		m.Row(4, func() {
+			m.Col(4, func() {
+				err := m.FileImage("public/zapitel_logo.png", props.Rect{
+					Center:  true,
+					Percent: 200,
+				})
+				if err != nil {
+					fmt.Println(err)
+				}
+			})
+		})
+
+	})
+
+	m.Row(4, func() {
+		m.Col(8, func() {
+			m.Text(heading, props.Text{
+				Top:   0,
+				Style: consts.Bold,
+				Size:  15,
+				Align: consts.Center,
+				Color: getDarkGrayColor(),
+			})
+		})
+	})
+
+}
+
+func getDarkGrayColor() color.Color {
+	myColor := color.Color{
+		Red:   180,
+		Green: 180,
+		Blue:  180,
+	}
+
+	return myColor
 }
